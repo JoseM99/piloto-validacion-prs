3cfe90c43777d0a15d11f04d17930c38a0b52140
JoseM99 <43457072+JoseM99@users.noreply.github.com>
2026-07-31 15:47:03 -0500

    Delete tabla_buena.sql

--- tabla_buena.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE mb_silver_prod.mmff.m_party (
-    party_id STRING COMMENT 'identificador unico del cliente',
-    nom_cliente_dac STRING COMMENT 'nombre del cliente',
-    _ingestion_time TIMESTAMP COMMENT 'fecha de ingesta',
-    _processing_time TIMESTAMP COMMENT 'fecha de proceso'
-)
-COMMENT 'tabla maestra de clientes del modelo mmff'
-TBLPROPERTIES ('Frecuencia'='Mensual','Naturaleza'='Propia','Tipo_Tabla'='maestra','Data_Owner'='Andrea Vicuna','Dac'='SI')
