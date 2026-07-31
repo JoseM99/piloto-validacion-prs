c581b6d5f355d9ba5a5b8881b7dd3b3f2625a8b6
JoseM99 <43457072+JoseM99@users.noreply.github.com>
2026-07-31 09:30:22 -0500

    Add notebook_bueno.py for data processing

---
+++ notebook_bueno.py
@@ -0,0 +1,4 @@
+# notebook_bueno.py  (cumple los lineamientos)
+df = spark.sql("SELECT id, monto, codmes FROM calidad.gold.ventas")
+df = df.withColumn("fecrutina", current_timestamp())
+df.write.format("delta").partitionBy("codmes").saveAsTable("calidad.gold.ventas_out")
